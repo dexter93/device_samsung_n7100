ed1a10d1f4817dcf812521f6ee86b3d02c065002
Josue Rivera <prbassplayer@gmail.com>
2013-09-03 04:13:10 +0200

    Adding SELinux support based on I9300 implementation. Thanks CM
    Change-Id: I2c3b9e93d93b3b82f1e8cbb06a0187b1acc39e39
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,22 @@ TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := slim_n7100_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro/bin/arm-linux-gnueabihf-
 
+# Selinux
+BOARD_SEPOLICY_DIRS := \
+    device/samsung/n7100/selinux
+
+BOARD_SEPOLICY_UNION := \
+    device.te \
+    domain.te \
+    file.te \
+    file_contexts \
+    init.te \
+    mediaserver.te \
+    rild.te \
+    system.te \
+    ueventd.te \
+    wpa_supplicant.te
+
 # assert
 TARGET_OTA_ASSERT_DEVICE := t03g,n7100,GT-N7100
 
